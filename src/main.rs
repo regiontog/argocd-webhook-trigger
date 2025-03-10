bd47a951072ff065f595caf8efbde9bcc098345b
Erlend Tobiassen <erlend.tobiassen@gmail.com>
2025-03-10 08:45:52 +0100

    mvp

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
 use git2::{DiffOptions, Repository};
+use ureq::tls::TlsConfig;
+
 fn main() {
+    let mut insecure = false;
+
+    let mut args = Vec::new();
+
+    let mut arguments = std::env::args();
+
+    let _ = arguments.next();
+
+    for argument in arguments {
+        if argument == "-k" {
+            insecure = true;
+            continue;
+        }
+
+        args.push(argument);
+    }
+
     let repo = match Repository::open(".") {
         Ok(repo) => repo,
         Err(e) => panic!("failed to open: {}", e),
@@ -8,7 +27,11 @@ fn main() {
     // Should always be HEAD for any given local commit yeah?
     let git_ref = "HEAD";
 
-    let commit = repo.head().unwrap().peel_to_commit().unwrap();
+    let commit = repo
+        .head()
+        .expect("repo to contain a commit")
+        .peel_to_commit()
+        .unwrap();
 
     // New commit sha
     // git show --no-patch --pretty=%H
@@ -24,31 +47,75 @@ fn main() {
 
     // List of modified files
     // git diff --name-only HEAD^1
-    let modified = repo
-        .diff_tree_to_tree(
-            parent.ok().and_then(|p| p.tree().ok()).as_ref(),
-            commit.tree().ok().as_ref(),
-            Some(&mut DiffOptions::new()),
-        )
-        .unwrap();
+    let mut modified = Vec::new();
 
-    modified
-        .print(git2::DiffFormat::NameOnly, |delta, hunk, line| {
-            println!("{:?}", delta.old_file().path());
-            true
-        })
-        .unwrap();
+    repo.diff_tree_to_tree(
+        parent.ok().and_then(|p| p.tree().ok()).as_ref(),
+        commit.tree().ok().as_ref(),
+        Some(&mut DiffOptions::new()),
+    )
+    .unwrap()
+    .print(git2::DiffFormat::NameOnly, |delta, _, _| {
+        if let Some(path) = delta.old_file().path() {
+            modified.push(path.to_string_lossy().to_string());
+        }
+
+        true
+    })
+    .unwrap();
 
     // No real equivalent for local repo, but maybe the active(HEAD) branch does the trick?
     let default_branch = repo.head().unwrap();
 
     // What ArgoCD knows the git repo that was modified as
-    let repo = "";
+    let repo = &args[0];
+    let argocd_server = &args[1];
 
-    println!("{}", git_ref);
-    println!("{}", before);
-    println!("{}", after);
-    println!("{}", "");
-    println!("{}", default_branch.name().unwrap());
-    println!("{}", repo);
+    // Call ArgoCD pretending to be a GitHub Event
+    // curl -H "Content-Type: application/json" -H 'X-GitHub-Event: push' --data '{ "ref": {{ .REF | toJson }}, "before": {{ .BEFORE | toJson }}, "after": {{ .AFTER | toJson }}, "repository": { "html_url": {{ .REPO | toJson }}, "default_branch": {{ .DEFAULT_BRANCH | toJson }} }, "commits": [ { "modified": [{{ .MODIFIED | toJson }}] } ] }' https://argocd.localho.st:8443/api/webhook
+    let agent = ureq::Agent::config_builder()
+        .tls_config(TlsConfig::builder().disable_verification(insecure).build())
+        .build()
+        .new_agent();
+
+    println!(
+        "{:?}",
+        agent
+            .post(&format!("{}/api/webhook", argocd_server))
+            .header("X-GitHub-Event", "push")
+            .send_json(&GitInfo {
+                _ref: git_ref.to_string(),
+                before,
+                after,
+                repository: Repo {
+                    html_url: repo.to_string(),
+                    default_branch: default_branch.name().unwrap().to_string(),
+                },
+                commits: vec![Commit {
+                    modified: vec![modified.join("\n")],
+                }],
+            })
+            .expect("to send webhook")
+    );
+}
+
+#[derive(serde::Serialize)]
+struct GitInfo {
+    #[serde(rename = "ref")]
+    _ref: String,
+    before: String,
+    after: String,
+    repository: Repo,
+    commits: Vec<Commit>,
+}
+
+#[derive(serde::Serialize)]
+struct Repo {
+    html_url: String,
+    default_branch: String,
+}
+
+#[derive(serde::Serialize)]
+struct Commit {
+    modified: Vec<String>,
 }
